558c2e7f91e553f7ba6acfe36b16e23b2f71d4e2
mrzhov <danil.ryzhov21@yandex.ru>
2025-02-26 16:22:22 +0300

    refactored

--- pkg/task/service.go
+++ pkg/task/service.go
@@ -4,9 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"strconv"
 
-	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
@@ -27,28 +25,22 @@ func (s Service) GetList(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(tasks)
 }
 
-type CreateBody struct {
-	Title  *string `json:"title"`
-	IsDone bool    `json:"is_done"`
-}
-
 func (s Service) Create(w http.ResponseWriter, r *http.Request) {
-	body := CreateBody{}
+	body := &TaskBody{}
+	bodyErr := _ComputeBody(w, r, body)
 
-	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
+	if bodyErr != nil {
 		return
 	}
 
-	if body.Title == nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Error: Field \"title\" is required!")
-		return
-	}
 	var task Task
 	task.Title = *body.Title
-	task.IsDone = body.IsDone
+
+	if body.IsDone != nil {
+		task.IsDone = *body.IsDone
+	} else {
+		task.IsDone = false
+	}
 
 	if res := s.db.Create(&task); res.Error != nil {
 		w.WriteHeader(http.StatusInternalServerError)
@@ -59,26 +51,6 @@ func (s Service) Create(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(task)
-}
-
-func (s Service) _GetById(w http.ResponseWriter, r *http.Request) (*Task, error) {
-	id, err := strconv.Atoi(mux.Vars(r)["id"])
-
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
-		return nil, err
-	}
-
-	var task Task
-
-	if res := s.db.First(&task, id); res.Error != nil {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "Error: Task not found")
-		return nil, res.Error
-	}
-
-	return &task, nil
 }
 
 func (s Service) GetById(w http.ResponseWriter, r *http.Request) {
@@ -92,29 +64,12 @@ func (s Service) GetById(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(*task)
 }
 
-type PatchBody struct {
-	Title  *string `json:"title"`
-	IsDone *bool   `json:"is_done"`
-}
-
 func (s Service) Patch(w http.ResponseWriter, r *http.Request) {
+	body := &TaskBody{}
+	bodyErr := _ComputeBody(w, r, body)
 	task, taskErr := s._GetById(w, r)
 
-	if taskErr != nil {
-		return
-	}
-
-	body := PatchBody{}
-
-	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, err)
-		return
-	}
-
-	if body.Title == nil {
-		w.WriteHeader(http.StatusBadRequest)
-		fmt.Fprint(w, "Error: Field \"title\" is required!")
+	if (bodyErr != nil) || (taskErr != nil) {
 		return
 	}
 
@@ -127,7 +82,7 @@ func (s Service) Patch(w http.ResponseWriter, r *http.Request) {
 	s.db.Save(task)
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(task)
+	json.NewEncoder(w).Encode(*task)
 }
 
 func (s Service) Delete(w http.ResponseWriter, r *http.Request) {
