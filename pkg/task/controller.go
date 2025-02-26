22a654b1300cc2d83086d3cc0340199a3c837e72
mrzhov <danil.ryzhov21@yandex.ru>
2025-02-26 15:41:55 +0300

    added GetById, Patch and Delete routes

--- pkg/task/controller.go
+++ pkg/task/controller.go
@@ -12,4 +12,7 @@ func Controller(r *mux.Router, db *gorm.DB) {
 
 	r.HandleFunc("/api/tasks", s.GetList).Methods(http.MethodGet)
 	r.HandleFunc("/api/tasks", s.Create).Methods(http.MethodPost)
+	r.HandleFunc("/api/tasks/{id}", s.GetById).Methods(http.MethodGet)
+	r.HandleFunc("/api/tasks/{id}", s.Patch).Methods(http.MethodPatch)
+	r.HandleFunc("/api/tasks/{id}", s.Delete).Methods(http.MethodDelete)
 }
--- pkg/task/service.go
+++ pkg/task/service.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"gorm.io/gorm"
 )
 
@@ -58,4 +60,94 @@ func (s Service) Create(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(task)
+}
+
+func (s Service) GetById(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	var task Task
+
+	if res := s.db.First(&task, id); res.Error != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, res.Error)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+}
+
+type PatchBody struct {
+	Title  *string `json:"title"`
+	IsDone *bool   `json:"is_done"`
+}
+
+func (s Service) Patch(w http.ResponseWriter, r *http.Request) {
+	id, idErr := strconv.Atoi(mux.Vars(r)["id"])
+	body := PatchBody{}
+
+	if idErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, idErr)
+		return
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	if body.Title == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Error: Field \"title\" is required!")
+		return
+	}
+
+	var task Task
+
+	if res := s.db.First(&task, id); res.Error != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, res.Error)
+		return
+	}
+
+	task.Title = *body.Title
+
+	if body.IsDone != nil {
+		task.IsDone = *body.IsDone
+	}
+
+	s.db.Save(&task)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(task)
+}
+
+func (s Service) Delete(w http.ResponseWriter, r *http.Request) {
+	id, idErr := strconv.Atoi(mux.Vars(r)["id"])
+
+	if idErr != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, idErr)
+		return
+	}
+
+	var task Task
+
+	if res := s.db.First(&task, id); res.Error != nil {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, res.Error)
+		return
+	}
+
+	s.db.Delete(&task)
+
+	fmt.Fprint(w, "Deleted successfully!")
 }
